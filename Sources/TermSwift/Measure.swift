e88561e6540c6d227262aa1c29ccc4e92f94d968
Bendik Solheim <hello@bendik.dev>
2021-04-14 21:14:30 +0200

    Make measure function return result

--- Sources/TermSwift/Measure.swift
+++ Sources/TermSwift/Measure.swift
@@ -1,10 +1,12 @@
 import Foundation
 import os.log
 
-public func measure(_ label: String, block: () -> Void) {
+public func measure<T>(_ label: String, block: () -> T) -> T {
     let info = ProcessInfo.processInfo
     let start = info.systemUptime
-    block()
+    let result = block()
     let diff = info.systemUptime - start
     os_log("%{public}@", "\(label): \(diff)s")
+    
+    return result
 }
