10ebab4ceca7e692c3bde9efa531ee9652860c0f
Bendik Solheim <hello@bendik.dev>
2022-08-23 22:17:45 +0200

    Parse byte 13 as enter

--- Sources/Slowbox/Keys.swift
+++ Sources/Slowbox/Keys.swift
@@ -4,5 +4,6 @@ let ctrlKeys = (UInt8(ascii: "a") ... UInt8(ascii: "z")).map { $0 & 0b0001_1111 
 
 let specialKeys: Dictionary<UInt8, KeyEvent> = [
     0x09: .tab,
-    0x1B: .esc
+    0x1B: .esc,
+    13: .enter
 ]
